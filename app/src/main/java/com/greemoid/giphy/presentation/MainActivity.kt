724108240a32911576309f2eccd3291067f9f2e8
greemoid <greemoiddd@gmail.com>
2022-07-14 12:27:51 +0300

    Initial commit

---
+++ app/src/main/java/com/greemoid/giphy/presentation/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.greemoid.giphy.presentation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.greemoid.giphy.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
